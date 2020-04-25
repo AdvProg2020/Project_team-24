27f1af16684f77a7fbd11d9fd59b868cda503307
Alireza <Qre.Alireza@gmail.com>
2020-04-25 22:50:17 +0430

    ...

--- src/main/java/View/Views/Menus/InPut.java
+++ src/main/java/View/Views/Menus/InPut.java
@@ -1,7 +1,11 @@
-package View.Menu;
+package View.Views.Menus;
+
+import View.Menu.OutPut;
 
 public class InPut {
-    protected OutPut consoleOutput =new OutPut();
+
+    protected OutPut consoleOutput = new OutPut();
+
     public void start(){
 
     }
