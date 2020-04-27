6835db36160b10460c9d1007cd79aaa313cd11ec
yasamingol <yasamingolzar.ce@gmail.com>
2020-04-27 06:22:35 -0700

    yac:)

--- src/main/java/Controller/Controllers/RegisterController.java
+++ src/main/java/Controller/Controllers/RegisterController.java
@@ -1,17 +1,25 @@
 package Controller.Controllers;
 
 import Controller.Exceptions.AccountExistanceException;
-import Controller.Exceptions.RegisterException;
+import Controller.Exceptions.TypeInvalidException;
 import Controller.Exceptions.UserNameInvalidException;
 import Controller.Exceptions.UserNameTooShortExcepton;
-import Model.Models.CompanyInformation;
 import Model.Models.PersonalInfo;
 
 import java.util.regex.Matcher;
 
 public class RegisterController {
-    public void creatTheBaseOfAccount(String type, String username) throws UserNameInvalidException, UserNameTooShortExcepton,TypeNotPresentException, AccountExistanceException{
-        Matcher
+    public void creatTheBaseOfAccount(String type, String username) throws UserNameInvalidException, UserNameTooShortExcepton, TypeInvalidException, AccountExistanceException{
+        Matcher registerUserNameMatcher = EssentialMethods.getMatcher("^\\w+$",username);
+        Matcher registerTypeMatcher = EssentialMethods.getMatcher("^Customer$|^Guest$|^Manager$|^Seller$",type);
+        if(registerUserNameMatcher.find()){
+            if(registerUserNameMatcher.group(0).matches("^\\w{6,}$")){
+                if(registerTypeMatcher.find()){
+                    //+m checkAccountExistance(username) throws .....
+                }else throw new TypeInvalidException("");
+            }else throw new UserNameTooShortExcepton("");
+        }else throw new UserNameInvalidException("");
+
     }
 
 
