913c8b9a8febfbdeed5d1f8508b797e08bcd078d
yasamingol <yasamingolzar.ce@gmail.com>
2020-04-27 06:54:07 -0700

    yac:)

--- src/main/java/Controller/Controllers/RegisterController.java
+++ src/main/java/Controller/Controllers/RegisterController.java
@@ -1,9 +1,7 @@
 package Controller.Controllers;
 
-import Controller.Exceptions.AccountExistanceException;
-import Controller.Exceptions.TypeInvalidException;
-import Controller.Exceptions.UserNameInvalidException;
-import Controller.Exceptions.UserNameTooShortExcepton;
+import Controller.Exceptions.*;
+import Model.Models.FieldList;
 import Model.Models.PersonalInfo;
 
 import java.util.regex.Matcher;
@@ -19,6 +17,30 @@ public class RegisterController {
                 }else throw new TypeInvalidException("");
             }else throw new UserNameTooShortExcepton("");
         }else throw new UserNameInvalidException("");
+
+    }
+    public void savePersonalInfo(String id,String firstName,String lastName,String email,String phoneNumber,FieldList fieldList) throws IdInvalidException,FirstNameInvalidException,LastNameInvalidException,EmailInvalidException,PhoneNumberInvalidException {
+        Matcher registerIdMatcher = EssentialMethods.getMatcher("^\\d+$",id);
+        Matcher registerFirstNameMatcher = EssentialMethods.getMatcher("^\\w+$",firstName);
+        Matcher registerLastNameMatcher = EssentialMethods.getMatcher("^\\w+$",lastName);
+        Matcher registerEmailMatcher = EssentialMethods.getMatcher("^\\w+@(gmail|yahoo)\\.com$",email);
+        Matcher registerPhoneNumMatcher = EssentialMethods.getMatcher("^\\d{11}$",phoneNumber);
+        if(registerIdMatcher.find()){
+            // Account.personalinfo.setid....
+        }else throw new IdInvalidException("");
+        if(registerFirstNameMatcher.find()){
+            // Account.personalinfo.setFirstname....
+        }else throw new FirstNameInvalidException("");
+        if(registerLastNameMatcher.find()){
+            // Account.personalinfo.setlastname....
+        }else throw new LastNameInvalidException("");
+        if(registerEmailMatcher.find()){
+            // Account.personalinfo.setemail....
+        }else throw new EmailInvalidException("");
+        if(registerPhoneNumMatcher.find()){
+            // Account.personalinfo.setphonenum....
+        }else throw  new PhoneNumberInvalidException("");
+
 
     }
 
---
+++ src/main/java/Controller/Exceptions/IdInvalidException.java
@@ -0,0 +1,8 @@
+package Controller.Exceptions;
+
+public class IdInvalidException extends Exception {
+    public IdInvalidException(String message){
+        super(message);
+
+    }
+}
