000c02452e8d54517addfab7ced8bc82fc69410a
yasamingol <yasamingolzar.ce@gmail.com>
2020-04-27 07:04:09 -0700

    yac:)

--- src/main/java/Controller/Controllers/RegisterController.java
+++ src/main/java/Controller/Controllers/RegisterController.java
@@ -40,9 +40,29 @@ public class RegisterController {
         if(registerPhoneNumMatcher.find()){
             // Account.personalinfo.setphonenum....
         }else throw  new PhoneNumberInvalidException("");
-
+        //save fieldlist
 
     }
+    public void saveCompanyInfo(String companyId,String name,String phoneNumber,String email,String foundation,FieldList fieldList) throws CompanyIdinvalidException,CompanyNameInvalidException,PhoneNumberInvalidException,EmailInvalidException{
+        Matcher registerCompanyIdMatcher = EssentialMethods.getMatcher("^\\d+$",companyId);
+        Matcher registerNameMatcher = EssentialMethods.getMatcher("^\\w+$",name);
+        Matcher registerEmailMatcher = EssentialMethods.getMatcher("^\\w+@(gmail|yahoo)\\.com$",email);
+        Matcher registerPhoneNumMatcher = EssentialMethods.getMatcher("^\\d{8}$",phoneNumber);
+        if(registerCompanyIdMatcher.find()){
+            // Account.personalinfo.setid....
+        }else throw new CompanyIdinvalidException("CompanyIdinvalidException");
+        if(registerNameMatcher.find()){
+            // Account.personalinfo.setname....
+        }else throw new CompanyNameInvalidException("CompanyNameInvalidException");
+        if(registerEmailMatcher.find()){
+            // Account.personalinfo.setemail....
+        }else throw new EmailInvalidException("EmailInvalidException");
+        if(registerPhoneNumMatcher.find()){
+            // Account.personalinfo.setphonenum....
+        }else throw  new PhoneNumberInvalidException("PhoneNumberInvalidException");
+        //save fieldlist
+    }
+
 
 
 }
---
+++ src/main/java/Controller/Exceptions/CompanyIdinvalidException.java
@@ -0,0 +1,9 @@
+package Controller.Exceptions;
+
+public class CompanyIdinvalidException extends Exception {
+    public CompanyIdinvalidException(String message){
+        super(message);
+
+    }
+}
+
