d729837c46a0ab43fd2007c751fe9ac511ef6a1a
Alireza <Qre.Alireza@gmail.com>
2020-04-28 02:10:52 +0430

    Sogolism.02

--- src/main/java/View/Views/Menus/ManagerMenu.java
+++ src/main/java/View/Views/Menus/ManagerMenu.java
@@ -1,6 +1,10 @@
 package View.Views.Menus;
 
+import Controller.Controllers.ManagerController;
+
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ManagerMenu extends Menu {
 
@@ -19,15 +23,26 @@ public class ManagerMenu extends Menu {
 
     // Tavabe
     public void createNewManager(){
-        System.out.println("enter your name");
-        String name;
-        name=scanner.nextLine().trim();
-        if(!name.matches("[a-zA-Z ]+")){
-            System.out.println("khafe shoooooooooooooooo");
-            return;;
+
+        System.out.print("enter Information in correct pattern :");
+        String info = scanner.nextLine().trim();
+        Matcher matcher = Pattern.compile("information  :(\\w+) :(\\w+) :(\\w+) :(\\w+) :(\\w+)").matcher(info);
+        if (!matcher.find()) {
+            System.out.println("Sogol khare ...");
+            return;
         }
+        String f_name = matcher.group(1);
+        String l_name = matcher.group(2);
+        String email = matcher.group(3);
+        String pNumber = matcher.group(4);
+        String pass = matcher.group(5);
 
-
+//        try {
+//        new ManagerController().creatManageProfile(f_name,l_name,email,pNumber,pass);
+//        } catch (ManagerRidException e) {
+//            System.out.println("Chon sogol khare dlil nmishe manager gand bzane ...");
+//            System.out.println("Rasti chone sogol jooosh zade");
+//        }
     }
 
     @Override
