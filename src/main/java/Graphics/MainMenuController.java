4794f06bc0671575acf043aeb4ba1b31b824089f
Alireza <Qre.Alireza@gmail.com>
2020-06-15 00:37:44 +0430

    ReBuild

--- src/main/java/Graphics/MainMenuController.java
+++ src/main/java/Graphics/MainMenuController.java
@@ -1,16 +1,48 @@
 package Graphics;
 
-import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class MainMenuController {
 
-    public TextField searchBar;
-    public Button bag;
-    public Pane pane2;
-    public Pane pane1;
-    public Button auction;
-    public Pane pane3;
-    public Button products;
+    public TextField searchArea;
+    public Pane auctionPane;
+    public Pane popularsPane;
+    public Pane productsPane;
+    public Pane aboutPane;
+
+    public void doCeSuT() {
+
+    }
+
+    public void goLogin() {
+
+    }
+
+    public void goAuction() {
+
+    }
+
+    public void goProducts() {
+
+    }
+
+    public void goCart() {
+
+    }
+
+    public void goPopulars() {
+
+    }
+
+    public void goSearch() {
+
+    }
+
+    private void changeSubScene(Scene newScene) {
+        //?
+    }
 }
