1a84b0e431ea36d5be623813b222909028c1bfb7
Alireza <Qre.Alireza@gmail.com>
2020-06-14 23:06:33 +0430

    ReBuild

--- src/main/java/Graphics/MainMenuController.java
+++ src/main/java/Graphics/MainMenuController.java
@@ -1,4 +1,16 @@
 package Graphics;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class MainMenuController {
+
+    public TextField searchBar;
+    public Button bag;
+    public Pane pane2;
+    public Pane pane1;
+    public Button auction;
+    public Pane pane3;
+    public Button products;
 }
