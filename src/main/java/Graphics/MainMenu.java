42998298dd7de97f8a1d1598f8f92cdb8a491b6a
Alireza <Qre.Alireza@gmail.com>
2020-06-24 15:48:44 +0430

    edit PriductCart

--- src/main/java/Graphics/MainMenu.java
+++ src/main/java/Graphics/MainMenu.java
@@ -67,7 +67,7 @@ public class MainMenu extends Application implements SceneBuilder, Initializable
     public Scene sceneBuilder() {
 
         try {
-            return FXMLLoader.load(new File("src\\main\\resources\\Graphics\\AuctionMenu\\AuctionMenu.fxml").toURI().toURL());
+            return FXMLLoader.load(new File("src/main/resources/Graphics/Product/Product.fxml").toURI().toURL());
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
--- src/main/java/Graphics/Product.java
+++ src/main/java/Graphics/Product.java
@@ -22,77 +22,6 @@ import java.util.Stack;
 
 public class Product implements Initializable, PaneBuilder {
 
-    private static Stack<Model.Models.Product> productStack = new Stack<>();
-    public Tab media;
-    public Tab similarProducts;
-    private Model.Models.Product product;
-
-    @FXML
-    private TableColumn<?, ?> ProductInfoName;
-    @FXML
-    private TableColumn<?, ?> ProductInfoValue;
-    @FXML
-    private TableColumn<?, ?> CategoryInfoName;
-    @FXML
-    private TableColumn<?, ?> CategoryInfo;
-    @FXML
-    private Tab comment;
-    @FXML
-    private Pane CommentCart;
-    @FXML
-    private Pane CommentCart1;
-    @FXML
-    private Pane CommentCart2;
-    @FXML
-    private Button newComment;
-    @FXML
-    private AnchorPane feature;
-    @FXML
-    private Label productName;
-    @FXML
-    private Pane stars;
-    @FXML
-    private ImageView star1;
-    @FXML
-    private ImageView star2;
-    @FXML
-    private ImageView star3;
-    @FXML
-    private ImageView star4;
-    @FXML
-    private ImageView star5;
-    @FXML
-    private Label price;
-    @FXML
-    private MediaView productVideo;
-    @FXML
-    private Button videoButton;
-    @FXML
-    private TextField searchBar;
-    @FXML
-    private ImageView productImage;
-    @FXML
-    private Pane similar1;
-    @FXML
-    private Pane similar2;
-    @FXML
-    private Pane similar3;
-    @FXML
-    private Pane similar4;
-    @FXML
-    private Pane similar5;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-        if (!productStack.isEmpty()) {
-            product = productStack.lastElement();
-            setImages();
-            setMedias();
-        }
-        similarPr();
-    }
-
     @Override
     public Pane paneBuilder() {
 
@@ -105,48 +34,8 @@ public class Product implements Initializable, PaneBuilder {
         return null;
     }
 
-    public void newComment() {
-    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-    public void playVideo() {
-    }
-
-    public void compare() {
-    }
-
-    public void openCart() {
-    }
-
-    private void setImages() {
-
-        productImage.setImage(product.getProductImage());
-
-        Image filledStar = new Image(new File("src/main/resources/Graphics/Product/icons8-star-filled-16.png").toURI().toString());
-
-        switch ((int) product.getAverageScore()) {
-            case 5:
-                star5.setImage(filledStar);
-            case 4:
-                star4.setImage(filledStar);
-            case 3:
-                star3.setImage(filledStar);
-            case 2:
-                star2.setImage(filledStar);
-            case 1:
-                star1.setImage(filledStar);
-        }
-
-    }
-
-    private void setMedias() {
-        productVideo = new MediaView(new MediaPlayer(product.getProductMedia()));
-    }
-
-    private void similarPr() {
-//        similar1.setPrefSize(130,100);
-//        similar2.setPrefSize(130,100);
-//        similar3.setPrefSize(130,100);
-//        similar4.setPrefSize(130,100);
-//        similar5.setPrefSize(130,100);
     }
 }
