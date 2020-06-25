8fb9de6602a9edf48ab7a9722c33cc679bf193d0
yasamingol <yasamingolzar.ce@gmail.com>
2020-06-25 09:15:55 -0700

    Graphics :)

--- src/main/java/Graphics/MainMenu.java
+++ src/main/java/Graphics/MainMenu.java
@@ -70,7 +70,7 @@ public class MainMenu extends Application implements SceneBuilder, Initializable
     public Scene sceneBuilder() {
 
         try {
-            return FXMLLoader.load(new File("src\\main\\resources\\Graphics\\Account\\CustomerAccount.fxml").toURI().toURL());
+            return FXMLLoader.load(new File("src\\main\\resources\\Graphics\\Account\\ManagerAccount.fxml").toURI().toURL());
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
