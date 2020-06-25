6c464a36aa6c6c9df3921cc5f447b9f7c4276d52
Alireza <Qre.Alireza@gmail.com>
2020-06-25 19:29:49 +0430

    graphic update

--- src/main/java/Graphics/Creates/CreateProduct.java
+++ src/main/java/Graphics/Creates/CreateProduct.java
@@ -2,6 +2,8 @@ package Graphics.Creates;
 
 import Graphics.Tools.SceneBuilder;
 import Model.Models.Field.Field;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
@@ -9,32 +11,53 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CreateProduct implements SceneBuilder, Initializable {
 
-    public TextField product_name;
-    public TextField product_price;
-    public TextField product_number;
-    public ImageView product_image;
-    public TextField category_feature;
-    public TextField category_value;
-    public TextField product_feature;
-    public TextField product_value;
-    public TableView<Field> category_table;
-    public TableView<Field> product_table;
-    public ChoiceBox<String> auction;
-    public ChoiceBox<String> category;
+    @FXML
+    private TextField product_name;
+    @FXML
+    private TextField product_price;
+    @FXML
+    private TextField product_number;
+    @FXML
+    private ImageView product_image;
+    @FXML
+    private TextField category_feature;
+    @FXML
+    private TextField category_value;
+    @FXML
+    private TextField product_feature;
+    @FXML
+    private TextField product_value;
+    @FXML
+    private TableView<Field> category_table;
+    @FXML
+    private TableView<Field> product_table;
+    @FXML
+    private ChoiceBox<String> auction;
+    @FXML
+    private ChoiceBox<String> category;
 
     @Override
     public Scene sceneBuilder() {
+
+        try {
+            return FXMLLoader.load(new File("src/main/resources/Graphics/CreatePages/CreateProduct/CreateProduct.fxml").toURI().toURL());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
         return null;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        category.
     }
 
     public void final_submit() {
