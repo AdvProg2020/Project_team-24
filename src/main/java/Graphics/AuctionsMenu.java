73e0fa2abc42547e9c54e61970d9c21ac5cd5f95
yasamingol <yasamingolzar.ce@gmail.com>
2020-06-26 10:25:25 -0700

    Graphics :)

---
+++ src/main/java/Graphics/AuctionsMenu.java
@@ -0,0 +1,13 @@
+package Graphics;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class AuctionsMenu {
+
+    public void nextPage(ActionEvent event) {
+    }
+
+    public void previosPage(ActionEvent event) {
+    }
+}
