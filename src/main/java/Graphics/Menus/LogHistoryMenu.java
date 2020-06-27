27d677512ef02bd05a75783049a17e4f3bfa8946
yasamingol <yasamingolzar.ce@gmail.com>
2020-06-27 05:30:04 -0700

    Graphics :)

--- src/main/java/Graphics/Menus/LogHistoryMenu.java
+++ src/main/java/Graphics/Menus/LogHistoryMenu.java
@@ -1,11 +1,15 @@
 package Graphics.Menus;
 
+import Graphics.MainMenu;
+import Graphics.Models.ProductCart;
 import Graphics.Tools.SceneBuilder;
 import Model.Models.LogHistory;
-import javafx.event.ActionEvent;
+import Model.Models.Product;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,7 +20,12 @@ import java.util.ResourceBundle;
 
 public class LogHistoryMenu implements SceneBuilder, Initializable {
 
-    private static List<LogHistory> logHistoryList = new ArrayList<>();
+    private static List<LogHistory> list = new ArrayList<>();
+    private static int pageNum = 1;
+    @FXML
+    private Button next_btn;
+    @FXML
+    private Button previous_btn;
 
     @Override
     public Scene sceneBuilder() {
@@ -30,17 +39,45 @@ public class LogHistoryMenu implements SceneBuilder, Initializable {
     }
 
     public static void setLogHistoryList(List<LogHistory> logHistoryList) {
-        LogHistoryMenu.logHistoryList = logHistoryList;
+        LogHistoryMenu.list = logHistoryList;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        //?
     }
 
-    public void nextPage(ActionEvent event) {
+    public void next() {
+        List<Product> list = new ArrayList<>();
+        for (int i = (++pageNum - 1) * 9; i < pageNum * 9 && i < list.size(); i++) {
+            list.add(list.get(i));
+        }
+        checkButtons();
+        ProductCart.setProductList(list);
+        MainMenu.change(new ProductsMenu().sceneBuilder());
     }
 
-    public void previousPage(ActionEvent event) {
+    public void previous() {
+        List<Product> list = new ArrayList<>();
+        for (int i = (--pageNum - 1) * 9; i < pageNum * 9 && i < list.size(); i++) {
+            list.add(list.get(i));
+        }
+        checkButtons();
+        ProductCart.setProductList(list);
+        MainMenu.change(new ProductsMenu().sceneBuilder());
+    }
+
+    private void checkButtons() {
+
+        if (pageNum == list.size() / 9 + 1) {
+            next_btn.setDisable(true);
+            return;
+        }
+        if (pageNum == 1) {
+            previous_btn.setDisable(true);
+            return;
+        }
+        next_btn.setDisable(false);
+        previous_btn.setDisable(false);
     }
 }
