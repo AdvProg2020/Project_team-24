04b667cfddd1fac9c8b1ba693d4c43233aab3743
yasamingol <yasamingolzar.ce@gmail.com>
2020-06-27 11:26:25 -0700

    final touches

--- src/main/java/Graphics/MainMenu.java
+++ src/main/java/Graphics/MainMenu.java
@@ -98,7 +98,8 @@ public class MainMenu extends Application implements SceneBuilder, Initializable
     public Scene sceneBuilder() {
 
         try {
-            return FXMLLoader.load(new File("src/main/resources/Graphics/MainMenu/MainMenu.fxml").toURI().toURL());
+            return FXMLLoader.load(new File("src\\main\\resources\\Graphics\\Sort\\Sort.fxml").toURI().toURL());
+           // return FXMLLoader.load(new File("src/main/resources/Graphics/MainMenu/MainMenu.fxml").toURI().toURL());
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
--- src/main/java/Graphics/Sort.java
+++ src/main/java/Graphics/Sort.java
@@ -2,6 +2,7 @@ package Graphics;
 
 import Controller.Controllers.FilterController;
 import Controller.Controllers.ProductsController;
+import Exceptions.NotAvailableSortException;
 import javafx.event.ActionEvent;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
@@ -17,16 +18,54 @@ public class Sort {
 
 
     public void Point(ActionEvent event) {
+        if(Point.isSelected()) {
 
+            try {
+                productsController.sort("Point");
+                System.out.println("sort point");
+            } catch (NotAvailableSortException e) {
+                e.printStackTrace();
+            }
+            NumberOfViews.setDisable(true);
+            TimeOfUpload.setDisable(true);
+        }else{
+            NumberOfViews.setDisable(false);
+            TimeOfUpload.setDisable(false);
 
+        }
 
     }
 
     public void NumberOfViews(ActionEvent event) {
+        if(NumberOfViews.isSelected()) {
+            try {
+                productsController.sort("NumberOfVisits");
+                System.out.println("sort number");
+            } catch (NotAvailableSortException e) {
+                e.printStackTrace();
+            }
+            Point.setDisable(true);
+            TimeOfUpload.setDisable(true);
+        }else{
+            Point.setDisable(false);
+            TimeOfUpload.setDisable(false);
+        }
 
     }
 
     public void TimeOfUpload(ActionEvent event) {
-
+        if(TimeOfUpload.isSelected()) {
+            try {
+                productsController.sort("Time");
+                System.out.println("sort time");
+            } catch (NotAvailableSortException e) {
+                e.printStackTrace();
+            }
+            Point.setDisable(true);
+            NumberOfViews.setDisable(true);
+        }else{
+            Point.setDisable(false);
+            NumberOfViews.setDisable(false);
+        }
     }
 }
