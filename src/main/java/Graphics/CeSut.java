9b970030cb8c6e12ccaeea81cc80e5bca89cdb93
Alireza <Qre.Alireza@gmail.com>
2020-06-27 18:01:32 +0430

    graphic update

--- src/main/java/Graphics/CeSut.java
+++ src/main/java/Graphics/CeSut.java
@@ -1,4 +1,23 @@
 package Graphics;
 
-public class CeSut {
+import Graphics.Tools.SceneBuilder;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CeSut implements SceneBuilder {
+
+    @Override
+    public Scene sceneBuilder() {
+        try {
+            return FXMLLoader.load(new File("src/main/resources/Graphics/AboutCeSut/AboutCeSut.fxml").toURI().toURL());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return null;
+    }
 }
