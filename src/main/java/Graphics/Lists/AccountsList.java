4738cc37a336f9cb6e9bf3b468b259b21bfc2377
yasamingol <yasamingolzar.ce@gmail.com>
2020-06-27 02:42:21 -0700

    Graphics :)

--- src/main/java/Graphics/Lists/AccountsList.java
+++ src/main/java/Graphics/Lists/AccountsList.java
@@ -1,9 +1,14 @@
 package Graphics.Lists;
 
 import Graphics.Tools.SceneBuilder;
+import Model.Models.Account;
+import Model.Models.Product;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.Pane;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +16,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AccountsList implements SceneBuilder, Initializable {
+    @FXML
+    public TableColumn<Account, > ;
     @Override
     public Scene sceneBuilder() {
         try {
