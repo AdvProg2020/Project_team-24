d6b234e316e185720b3aa16c2a5a68af9c228d93
yasamingol <yasamingolzar.ce@gmail.com>
2020-06-27 10:38:59 -0700

    final touches

--- src/main/java/Graphics/Sort.java
+++ src/main/java/Graphics/Sort.java
@@ -1,18 +1,31 @@
 package Graphics;
 
+import Controller.Controllers.FilterController;
+import Controller.Controllers.ProductsController;
 import javafx.event.ActionEvent;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 
 public class Sort {
+    public FilterController filterController = FilterController.getInstance();
+    public ProductsController productsController = ProductsController.getInstance();
     public CheckBox Point;
     public CheckBox TimeOfUpload;
+    public CheckBox NumberOfViews;
+    public ChoiceBox productInfo;
+    public ChoiceBox CategoryInfo;
+
 
     public void Point(ActionEvent event) {
+        productsController.sort("Point");
+
     }
 
     public void NumberOfViews(ActionEvent event) {
+        productsController.sort("NumberOfVisits");
     }
 
     public void TimeOfUpload(ActionEvent event) {
+        productsController.sort("Time");
     }
 }
