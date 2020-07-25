64dc1488088711739965165898e7daf668893c8b
yasamingol <yasamingolzar.ce@gmail.com>
2020-07-25 18:56:04 +0430

    :)

--- src/main/java/A_Client/Graphics/ChatArea/ChatArea.java
+++ src/main/java/A_Client/Graphics/ChatArea/ChatArea.java
@@ -127,4 +127,10 @@ public class ChatArea implements Initializable {
         m.setTextAlignment(right);
         vBox.setAlignment(centerRight);
     }
+
+    public static YacGram getYacGram() {
+        return yacGram;
+    }
+
+
 }
--- src/main/java/A_Client/Graphics/Pages/ViewAllSupporters.java
+++ src/main/java/A_Client/Graphics/Pages/ViewAllSupporters.java
@@ -3,19 +3,33 @@ package A_Client.Graphics.Pages;
 import A_Client.ChatClient.ChatRoom;
 import A_Client.ChatClient.YacGram;
 import A_Client.Client.SendAndReceive.SendAndReceive;
+import A_Client.Graphics.ChatArea.ChatArea;
 import A_Client.Graphics.MainMenu;
 import A_Client.Graphics.Tools.SceneBuilder;
 import Structs.MiniAccount;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class ViewAllSupporters implements SceneBuilder {
+public class ViewAllSupporters implements SceneBuilder, Initializable {
 
+
+    public TableView<MiniAccount> accountTableView;
+    public TableColumn<MiniAccount,Pane> buttons;
+    public TableColumn<MiniAccount, String> username;
 
     @Override
     public Scene sceneBuilder() {
@@ -27,11 +41,23 @@ public class ViewAllSupporters implements SceneBuilder {
         }
         return null;
     }
-    private Pane setChoicePane(MiniAccount account) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        init();
+    }
+
+    private void init() {
+        List<MiniAccount> list = SendAndReceive.getAllSupporters();
+        accountTableView.setItems(FXCollections.observableList(list));
+        username.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getUsername()));
+        buttons.setCellValueFactory(param -> new SimpleObjectProperty<>(setChoicePane(param.getValue().getUsername())));
+    }
+    private Pane setChoicePane(String username) {
         Button chatWithSupporter = new Button("گفت و گو");
         chatWithSupporter.setOnAction(event -> {
-            MainMenu.change(new ChatRoom());
+            ChatArea.getYacGram().join(username);
         });
+        return new Pane(chatWithSupporter);
     }
 
 }
